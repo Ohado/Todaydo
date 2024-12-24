d28d37be8dd47274d24e654a9be22c0ddbae0496
unknown <ohad578@gmail.com>
2024-12-24 20:57:26 +0200

    forgot to fix a small bug

--- store/store.js
+++ store/store.js
@@ -1,6 +1,5 @@
 import { todoReducer } from "./reducers/todo.reducer.js"
 import { userReducer } from "./reducers/user.reducer.js"
-import { styleReducer } from "./reducers/style.reducer.js"
 
 
 const { createStore, combineReducers, compose} = Redux
@@ -9,7 +8,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const appReducer = combineReducers({
     todoModule: todoReducer,
     userModule: userReducer,
-    styleModule: styleReducer,
 })
 
 export const store = createStore(appReducer, composeEnhancers()) //it's stuck here
